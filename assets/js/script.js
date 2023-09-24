bd95c1d922656417a9b871bb894ef462087aed2b
Hira Khan <phr.hirakhan@gmail.com>
2023-09-24 23:06:58 +0200

    js logic for winner written

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,40 @@ let currentPlayer = 0;
 
 const cells = document.getElementsByClassName("cell");
 
+checkWinner = () => {
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2] &&
+      board[i][0] !== ""
+    ) {
+      return board[i][0];
+    }
+    if (
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i] &&
+      board[0][i] !== ""
+    ) {
+      return board[0][i];
+    }
+  }
+  if (
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2] &&
+    board[0][0] !== ""
+  ) {
+    return board[0][0];
+  }
+  if (
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0] &&
+    board[0][2] !== ""
+  ) {
+    return board[0][2];
+  }
+  return "Nowin";
+};
+
 forEachCall = (element, i) => {
   element.addEventListener("click", function () {
     // Inside Click Event
@@ -26,7 +60,10 @@ forEachCall = (element, i) => {
     if (currentPlayer === 0) currentPlayer = 1;
     else currentPlayer = 0;
 
-    console.log(board);
+    // Check winner logic
+    winner = checkWinner();
+
+    console.log(winner);
   });
 };
 
