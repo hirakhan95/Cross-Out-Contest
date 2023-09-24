0f6bf7f52847045d958502ec81914cc8ff9e1d96
Hira Khan <phr.hirakhan@gmail.com>
2023-09-24 21:29:22 +0200

    js selecting forEach cell and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,9 @@ let board = [
 ];
 
 let currentPlayer = "X";
+
 const cells = document.querySelectorAll(".cell");
 
-function checkWinner(winner) {
-  return null;
-}
+cells.forEach((cell, index) => {
+  console.log(cell, index);
+});
