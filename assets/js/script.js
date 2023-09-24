958da66035c7204815fad4c8bc53a2235ab757d5
Hira Khan <phr.hirakhan@gmail.com>
2023-09-24 21:44:17 +0200

    js trying with various function types

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,36 @@ let currentPlayer = "X";
 
 const cells = document.querySelectorAll(".cell");
 
-cells.forEach((cell, index) => {
-  console.log(cell, index);
+// function clickEventOnCell(index) {
+//   const row = Math.floor(index / 3);
+//   const col = index % 3;
+//   if (board[row][col] === "") {
+//     board[row][col] = currentPlayer;
+//     cell.textContent = currentPlayer;
+//     const winner = checkWinner();
+//     if (winner) {
+//       showWinner(winner);
+//       setTimeout(resetBoard, 2000);
+//     } else {
+//       currentPlayer = currentPlayer === "X" ? "O" : "X";
+//     }
+//   }
+// }
+
+// function abc(asd, j) {
+//   console.log(asd, j);
+// }
+
+// cells.forEach(abc);
+
+a1 = (i, k, l) => {
+  console.log(i, k, l);
+};
+
+function a2(i) {
+  console.log(i);
+}
+
+[1, 2, 3].forEach(function (i) {
+  console.log(i);
 });
